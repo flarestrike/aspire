f1db635897124e7382b724c16b54bb75d1b8324d
bmVsc29u <bmVsc29u@gmail.com>
2019-06-12 21:05:02 +0800

    feat(lander): add qr code

--- src/lander/cover/cover.tag.ts
+++ src/lander/cover/cover.tag.ts
@@ -1,6 +1,16 @@
 import { Input, Component } from '@angular/core';
 import { InfoCard } from 'src/model';
 
+import * as Q from 'qrcode';
+
+const qopts = {
+  margin: 1,
+  color: {
+    dark: '#333',  // Blue dots
+    light: '#0000' // Transparent background
+  }
+};
+
 @Component({
   selector: 'ld-cover',
   templateUrl: './cover.tag.html',
@@ -10,7 +20,11 @@ export class CoverTag extends InfoCard {
   @Input() set data(v) {
     Object.assign(this, v);
   }
+  url = '';
   constructor() {
     super();
+    Q.toDataURL('asfdasdf', qopts).then(u => {
+      this.url = u;
+    });
   }
 }
