31f4c061154d75d372fcac94653905136158e885
bmVsc29u <bmVsc29u@gmail.com>
2019-06-16 12:13:18 +0800

    feat(app): update app icon after profile loaded

--- src/utils/profile.loader.ts
+++ src/utils/profile.loader.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { mergeMap, map } from 'rxjs/operators';
 import { Gtag } from '@chakray/utils/gtag';
@@ -6,6 +7,7 @@ import { Gtag } from '@chakray/utils/gtag';
 @Injectable()
 export class ProfileLoader {
   constructor(
+    @Inject(DOCUMENT) private doc,
     private gt: Gtag,
     private ar: ActivatedRoute) {
   }
@@ -14,8 +16,13 @@ export class ProfileLoader {
     return ar.queryParams.pipe(mergeMap(q => {
       this.gt.event('screen_view', { app_name: 'SPA', screen_name: q.hl });
       return ar.root.firstChild.data.pipe(map(({ profile: p }) => {
+        this.updateAppIcon(p.info.avatar);
         return { profile: p, query: q };
       }));
     }));
+  }
+  updateAppIcon(icon) {
+    const rel = this.doc.querySelector('link[rel="apple-touch-icon"]');
+    rel.href = icon;
   }
 }
